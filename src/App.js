60711a0e058325a6085e67c37b9f7fa767589db5
kejoperezde <kejoperezde@ittepic.edu.mx>
2025-05-18 23:12:49 -0600

    Preferencias arreglado

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/preferencias" element={<Preferencias />} />
 
         {/* Rutas protegidas (requieren autenticación) */}
         <Route element={<ProtectedRoute />}>
@@ -33,9 +34,9 @@ function App() {
         </Route>
 
         {/* Rutas solo para votantes */}
-        <Route element={<ProtectedRoute allowedRoles={["votante"]} />}>
+        {/* <Route element={<ProtectedRoute allowedRoles={["votante"]} />}>
           <Route path="/preferencias" element={<Preferencias />} />
-        </Route>
+        </Route> */}
 
         {/* Rutas solo para votantes */}
         <Route element={<ProtectedRoute allowedRoles={["candidato"]} />}>
